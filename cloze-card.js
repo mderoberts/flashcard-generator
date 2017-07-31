b606464fe8bd7e195d1fc7f908add16689b6771f
MB DR <mderoberts@users.noreply.github.com>
2017-07-30 22:35:35 -0400

    constructors defined

--- cloze-card.js
+++ cloze-card.js
@@ -1,3 +1,15 @@
 
 
-var ClozeCard = function()
+var ClozeCard = function(text, cloze) {
+    this.cloze = cloze;
+    this.partial = partial;
+    this.fullText = cloze + partial;
+    this.createCard = function(err, data){
+        if (!fulltext) {
+            console.log(err);
+        }
+
+    }
+};
+
+module.exports = ClozeCard;
