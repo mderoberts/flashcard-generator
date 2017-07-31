ec7f1b0eed22bc5d26ecfb19c5edab1664d37890
MB DR <mderoberts@users.noreply.github.com>
2017-07-30 23:10:44 -0400

    install packages, inquirer prompts

--- cloze-card.js
+++ cloze-card.js
@@ -1,15 +1,28 @@
-
+var inquirer = require("inquirer");
 
 var ClozeCard = function(text, cloze) {
     this.cloze = cloze;
     this.partial = partial;
-    this.fullText = cloze + partial;
-    this.createCard = function(err, data){
-        if (!fulltext) {
-            console.log(err);
+    this.fullText = fullText;
+    this.questions = inquirer.prompt([
+        {type: "input",
+        name: "card-text",
+        message: "Write a statement for this card."
+        }, {
+        type: "input",
+        name: "cloze-text",
+        message: "What word or phrase do you want to remove?"
         }
+    ]);
+    this.card = function(){
+        console.log(questions.inquirer.prompt[0].message);
+        
+        // if (!cloze) {
+        //     console.log(err);
+        // }
 
     }
 };
 
+card();
 module.exports = ClozeCard;
